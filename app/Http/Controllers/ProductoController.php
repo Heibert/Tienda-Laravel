cfb30e4ba94dbdc70d87a9d48b9d041e0c2d8e47
Heibert <hsmogollon8@misena.edu.co>
2022-05-26 17:42:45 -0500

    Validaciones

--- app/Http/Controllers/ProductoController.php
+++ app/Http/Controllers/ProductoController.php
@@ -6,6 +6,7 @@ use App\Models\Categoria;
 use App\Models\Marca;
 use App\Models\Producto;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\validator;
 
 class ProductoController extends Controller
 {
@@ -42,9 +43,48 @@ class ProductoController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(Request $r)
     {
-        
+        //Recibir datos
+/*         echo "<pre>";
+        var_dump($r->all());
+        echo "</pre>"; */
+
+        //Reglas de validacion de campos
+        $reglas = [
+            "nombre"=>'required|alpha',
+            "desc"=>'',
+            "precio"=>'numeric|required',
+            "marca"=>'required|numeric',
+            "categoria"=>'required|numeric'
+        ];
+        $mensajes = [
+            "required" => "El campo es obligatorio",
+            "alpha" => "Solo letras",
+            "numeric" => "Solo puede contener numeros"
+        ];
+        //Crear objeto de validacion
+        $validation = Validator::make($r->all(),$reglas,$mensajes);
+        //Validar los datos
+        if($validation->fails()){
+            return redirect('productos/create')
+            ->withErrors($validation)
+            ->withInput();
+        }
+        else{
+        //Crear nuevo producto<<entity>>
+        $p =new Producto;
+        //Asignar valores a los atributos
+        $p->nombre = $r->nombre;
+        $p->descripcion = $r->desc;
+        $p->precio = $r->precio;
+        $p->marca_id = $r->marca;
+        $p->categoria_id = $r->categoria;
+        //Guardar en la bd
+        $p->save();
+        return redirect('productos/create')
+            ->with('mensaje',"Producto registrado");
+        }
     }
 
     /**
--- resources/views/productos/registro.blade.php
+++ resources/views/productos/registro.blade.php
@@ -1,55 +1,67 @@
 @extends('layouts.menu')
 @section('contenido')
+@if(session('mensaje'))
+<div class="green-text">
+    <span><h4>{{session('mensaje')}}</h4></span>
+</div>
+@endif
 <div class="row">
     <h1 class="#f57c00 orange-text darken-2">Nuevo Producto</h1>
 </div>
 <div class="row">
-    <form method="POST" action="{{route('productos.store')}}" class="col s12" @csrf>
+    <form method="POST" action="{{route('productos.store')}}" class="col s12" >
+    @csrf
         <div class="row">
-            <div class="imput-field col s8">
+            <div class="input-field col s8">
+                <input placeholder="Nombre del producto" type="text" id="nombre" name="nombre" value="{{old('nombre')}}">
                 <label for="nombre">Nombre</label>
-                <input placeholder="Nombre del producto" type="text" id="nombre" name="nombre">
+                <div class="red-text">{{$errors->first('nombre')}}</div>
             </div>
         </div>
         <div class="row">
             <div class="input-field col s8">
                 <label for="desc">Descripcion</label>
-                <input placeholder="Descripcion del producto" type="text" name="desc" id="desc">
-                </textarea>
+                <textarea class="input-field" placeholder="Descripcion del producto" name="desc" id="desc">{{old('desc')}}</textarea>
+                <span class="red-text">{{$errors->first('desc')}}</span>
             </div>
         </div>
         <div class="row">
             <div class="imput-field col s8">
                 <label for="precio">Precio</label>
-                <input placeholder="Precio del producto" type="text" id="precio" name="precio">
+                <input placeholder="Precio del producto" type="number" id="precio" name="precio" value="{{old('precio')}}">
+                <span class="red-text">{{$errors->first('precio')}}</span>
             </div>
         </div>
         <div class="row">
             <div class="col s8 input-field">
                 <label for="marca">
-                Elija una marca
+                Marca
                 </label><br>
                 <select name="marca" id="marca">
+                <option value="">Elija una marca</option>
                     @foreach($marcas as $marca)
-                        <option value="">
+                        <option value="{{ $marca->id}}">
                             {{$marca->nombre}}
                         </option>
                     @endforeach
                 </select>
+                <span class="red-text">{{$errors->first('marca')}}</span>
             </div>
         </div>
         <div class="row">
             <div class="col s8 input-field">
                 <label for="categoria">
-                Elija una categoria
+                Categoria
                 </label><br>
                 <select name="categoria" id="categoria">
+                    <option value="">Elija una categoria</option>
                     @foreach($categorias as $categoria)
-                        <option value="">
+                        <option value="{{$categoria->id}}">
                             {{$categoria->nombre}}
                         </option>
                     @endforeach
                 </select>
+                <span class="red-text">{{$errors->first('categoria')}}</span>
             </div>
         </div>
         <div class="row">
